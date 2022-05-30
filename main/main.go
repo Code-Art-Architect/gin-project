7afa3f9499fe2fe991bf8941776309a83db0e49b
JettyFlutter3306 <1529146935@qq.com>
2022-05-30 21:38:22 +0800

    init

---
+++ main/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.LoadHTMLGlob("templates/**/*")
+
+	r.GET("/", func(context *gin.Context) {
+		context.HTML(200, "index.html", nil)
+	})
+
+	_ = r.Run()
+}
